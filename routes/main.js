27e61823ce839589ee5ead0425615ffbc6081442
Tamas Veres <veres454@gmail.com>
2019-02-08 16:57:42 +0100

    Finally, the game is working

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res){
-	res.render('../public/main.html');
+	res.render('../public/index.html');
 });
 
 module.exports = router;
