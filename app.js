db1db0c2086092bbedb06cebe45b76dbcee48d5b
Tamas Veres <veres454@gmail.com>
2019-02-10 22:33:53 +0100

    Added email to database, username to registration

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ var app = express();
 var path = require('path');
 
 const port = 3000;
-/*const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "pamacs2000",
-	port: "3306",
-//	database: ""
-})*/
 
 var main = require('./routes/main.js');
 var login = require('./routes/login.js');
@@ -24,27 +17,27 @@ app.set('view engine', 'html');
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+
+//IMPORTANT!!!!!!!!
+//RUN THIS CODE ONLY ONCE, THEN DELETE IT
+/*db.query('ALTER TABLE users ADD email VARCHAR(255)', function(err, result){
+	if (err) throw err;
+	console.log("email field added");
+});*/
+
+
+
+
 db.query('CREATE DATABASE IF NOT EXISTS MainDatabase', function(err){
 	if (err) throw err;
 	console.log("MainDatabase created");
-	var sql = 'CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))';
+	var sql = 'CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), email VARCHAR(255))';
 	db.query(sql, function (err, result){
 		if (err) throw err;
 		console.log("Users table created");
 	});
 });
 
-/*connection.connect(function(err){
-	if (err) throw err;
-	console.log("Database connected!");
-});*/
-
-/*app.get('/', function(req, res){
-	//res.send("Hello World");
-	res.sendFile(path.join(__dirname + '/index.html'));
-		
-});	*/
-//app.use(express.static('public'));
 app.use('/', main);
 app.use('/login', login);
 app.use('/register', register);
--- routes/register.js
+++ routes/register.js
@@ -11,11 +11,13 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
 	var registerEmail = req.body.registerEmail;
 	var registerPassword = req.body.registerPassword;
-	console.log("New registration, email: " + registerEmail + " password: " + registerPassword);
+	var registerUsername = req.body.registerUsername;
+	console.log("New registration, email: " + registerEmail + " password: " + registerPassword + " username: " + registerUsername);
 
 	/*Write the database query here
-	The registered email is stored iin registerEmail
-	The registered password s stored n regsterPassword */
+	The registered email is stored in registerEmail
+	The registered password is stored in regsterPassword
+	The registered username is stored in registerUsername */
 	//db.query()
 
 	res.render('../public/registerSuccess.html');
