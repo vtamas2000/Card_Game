6a73760891adf1e329bca4b9a60eb1f357916b93
Tamas Veres <veres454@gmail.com>
2019-02-10 21:45:53 +0100

    Registration solved

--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,23 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 var path = require('path');
+var db = require('../db.js');
 
 router.get('/', function(req, res){
 	res.render('../public/register.html');
+});
+
+router.post('/', function(req, res){
+	var registerEmail = req.body.registerEmail;
+	var registerPassword = req.body.registerPassword;
+	console.log("New registration, email: " + registerEmail + " password: " + registerPassword);
+
+	/*Write the database query here
+	The registered email is stored iin registerEmail
+	The registered password s stored n regsterPassword */
+	//db.query()
+
+	res.render('../public/registerSuccess.html');
 });
 
 module.exports = router;
