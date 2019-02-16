ce0fdb32c863f449d17a5286326f6296aebb8eef
hangyalpeter <hangyal.peter@gmail.com>
2019-02-16 22:27:49 +0100

    Clients can now play with eachother - not yet separated by rooms or sg. note: restart should be handled on both sides

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -7,7 +7,7 @@ var socket;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CORNER); //draw circles from their top left point
+  //ellipseMode(CORNER); //draw circles from their top left point
   textAlign(CENTER, CENTER);
   button = createButton("Restart");
   button.position(windowWidth/2, windowHeight);
@@ -68,65 +68,18 @@ var isdragging;
 function mouseDragged(event)
 {
    isdragging = event.returnValue;
-   //console.log(isdragging);
+  
 }
 
 function newPiece(data){
-  
-  for (let j = 0; j < h; j++) 
-  { 
-    
-     for (let i = 0; i < w; i++) 
-      {
-    
-        if (board[j][i]>0)
-        {
-          //fill(board[j][i]==1 ? 255 : 0, board[j][i]==2 ? 255 : 0, 0);
-          fill(0);
-          stroke(100)
-          textSize(bs/2)
-          text(board[j][i] == 1 ? 'X': 'O',i*bs, j*bs, bs, bs);
-        }
-      }
-  }
-  
-  
-}
 
-
-function mouseClicked() {
-  
-  let x = int(mouseX / bs), y = int(mouseY / bs) //y = nextSpace(x);
-  
-  var data = {
-    x: x,
-    y: y,
-    player: player
-  }
-  socket.emit('mouse', data);
-
-  if (y >= 0 && board[y][x] == 0 && !isdragging && !buttonclicked) 
+  if (data.y >= 0 && board[data.y][data.x] == 0 && !data.isdragging && !data.buttonclicked) 
   {
-    board[y][x] = player; 
-    player = player == 1 ? 2 : 1; //switch to the other player (1->2  2->1)
+    board[data.y][data.x] = data.player; 
+    data.player = data.player == 1 ? 2 : 1; //switch to the other player (1->2  2->1)
   }
-  
-}
 
-function RestartGame()
-{
-  for (let y = 0; y < h; y++)
-    for(let x = 0; x < w; x++)
-      board[y][x] = 0;
-  button.position(windowWidth/2, windowHeight);
-  button.size(100, 30);
-  let x = int(mouseX / bs), y = int(mouseY / bs)
-  board[y][x] = 0; 
-  player = 1;
-  buttonclicked = true;
-}
-
-function draw() {
+  //inserting draw here
 
   if (getWinner() == 0) 
   {
@@ -138,8 +91,8 @@ function draw() {
       
       for (let i = 0; i < w; i++) 
       {
-          fill(255);
-          rect(i*bs, j*bs, bs, bs);
+        fill(255);
+        rect(i*bs, j*bs, bs, bs);
 
           if (board[j][i]>0)
           {
@@ -160,9 +113,92 @@ function draw() {
     text("Player "+getWinner()+" Wins!", width/2,height/2);
     button.position(width/2 - 75,height/2 + 50);
     button.size(200, 50);
-    /*if(keyIsPressed && keyCode == ENTER)
-    {
-     RestartGame();
-  	} */ 	
   }
 }
+
+//////////////////////////////////////////////////////
+function mouseClicked() {
+  
+  let x = int(mouseX / bs), y = int(mouseY / bs) //y = nextSpace(x);
+  
+  var data = {
+    x: x,
+    y: y,
+    player: player,
+    isdragging: isdragging,
+    buttonclicked: buttonclicked
+  }
+  socket.emit('mouse', data);
+
+  if (y >= 0 && board[y][x] == 0 && !isdragging && !buttonclicked) 
+  {
+    board[y][x] = player; 
+    player = player == 1 ? 2 : 1; //switch to the other player (1->2  2->1)
+  }
+
+  //inserting draw here
+
+  if (getWinner() == 0) 
+  {
+    isdragging = false;
+    buttonclicked = false;
+
+    for (let j = 0; j < h; j++) 
+    {
+      
+      for (let i = 0; i < w; i++) 
+      {
+        fill(255);
+        rect(i*bs, j*bs, bs, bs);
+
+          if (board[j][i]>0)
+          {
+                //fill(board[j][i]==1 ? 255 : 0, board[j][i]==2 ? 255 : 0, 0);
+            		fill(0);
+            		stroke(100)
+            		textSize(bs/2)
+                text(board[j][i] == 1 ? 'X': 'O',i*bs, j*bs, bs, bs);
+          }
+    	}
+    }
+  }
+  else
+  {
+    background(0);
+    fill(255);
+    textSize(80);
+    text("Player "+getWinner()+" Wins!", width/2,height/2);
+    button.position(width/2 - 75,height/2 + 50);
+    button.size(200, 50);	
+  }
+  
+}
+/////////////////////////////////////////////////////////////////////
+function RestartGame()
+{
+  for (let y = 0; y < h; y++)
+    for(let x = 0; x < w; x++)
+      board[y][x] = 0;
+  
+  button.position(windowWidth/2, windowHeight);
+  button.size(100, 30);
+  let x = int(mouseX / bs), y = int(mouseY / bs)
+  board[y][x] = 0; 
+  player = 1;
+  buttonclicked = true;
+}
+
+var isdrawngrid;
+function draw() {
+ 
+  if(!isdrawngrid){
+   for (let j = 0; j < h; j++)   
+      for (let i = 0; i < w; i++) {
+        fill(255);
+         rect(i*bs, j*bs, bs, bs);
+       }     
+    isdrawngrid = true;
+ }
+  
+}
+
