d98eb36cf71761c865a2d63de7b53e6c4a5d3a54
Tamas Veres <veres454@gmail.com>
2019-06-12 17:16:02 +0200

    Mandelbrot finished

--- scripts/mandelbrot/mandelbrotSketch.js
+++ scripts/mandelbrot/mandelbrotSketch.js
@@ -1,21 +1,30 @@
-var boundary = 1;
+var boundX = 1;
+var boundY = 1;
 var size = 600;
 var limit = 10000000;
-var maxIterations = 1000;
+var maxIterations = 100;
 var diffX = 0;
 var diffY = 0;
+var mouseStartX;
+var mouseStartY;
+var mouseEndX;
+var mouseEndY;
 gradient = GradientGenerator.createGradient(['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#000000']);
 
 function setup(){
-    //createCanvas(600, 600);
+    //createCanvas(850, 850);
     createCanvas(windowWidth, windowHeight);
     background(0);
+    mandelbrot(diffX, diffY, boundX, boundY);
     //loadPixels();
 }
 
 function draw(){ 
-    mandelbrot(diffX, diffY);
-    noLoop();  
+    fill(color(0,0,0,0));
+    var w = mouseEndX - mouseStartX;
+    var h = mouseEndY - mouseStartY;
+    rect(mouseStartX, mouseStartY, w, h);
+    //noLoop();  
     /* for(var x = 0; x < width; x++){
         for(var y = 0; y < height; y++){
             //Complex number is a + bi
@@ -65,7 +74,7 @@ function draw(){
 } */
 
 function keyPressed(){
-    if(keyCode === UP_ARROW){
+    /* if(keyCode === UP_ARROW){
         boundary += 0.2;
     }
 
@@ -83,16 +92,45 @@ function keyPressed(){
 
     if(key == " "){
         mandelbrot(diffX, diffY);
-    }
+    } */
 }
 
-function mandelbrot(offsetX, offsetY){
+function mousePressed(){
+    mouseStartX = mouseX;
+    mouseStartY = mouseY;
+    console.log(width/height);
+}
+
+function mouseDragged(){
+    mouseEndX = mouseX;
+    //mouseEndY = mouseY;
+    mouseEndY = (mouseEndX - mouseStartX) / width * height + mouseStartY;
+}
+
+function mouseReleased(){
+    /* fill(255);
+    var w = mouseEndX - mouseStartX;
+    var h = mouseEndY - mouseStartY;
+    rect(mouseStartX, mouseStartY, w, h); */
+    //diffX = -(mouseStartX + (mouseEndX - mouseStartX) / 2) / width * bound + diffX;
+
+    //You have to put the middle not to the halfwidth of the rect, but scale it, as the boundaries are scaled!!!
+    var ratio = width / height;
+    diffX = -((mouseStartX + (mouseEndX - mouseStartX) / (2 * boundX + ratio * boundX) * (1 + ratio) * boundX) / width * boundX + diffX);
+    diffY = -((mouseStartY + (mouseEndY - mouseStartY) / 2) / height * boundY + diffY);
+    boundX = (mouseEndX - mouseStartX) / width * (2 * boundX + ratio * boundX) / (2 + ratio);
+    boundY = (mouseEndY - mouseStartY) / height * boundY;
+    mandelbrot(diffX, diffY, boundX, boundY);
+}
+
+function mandelbrot(offsetX, offsetY, boundaryX, boundaryY){
     for(var x = 0; x < width; x++){
         for(var y = 0; y < height; y++){
             //Complex number is a + bi
             //console.log(x + " " + y);
-            var a = map(x, 0, width, -2.5 * boundary, boundary)+offsetX;
-            var b = map(y, 0, height, -boundary, boundary)+offsetY;
+            var a = map(x, 0, width, -(1+width/height) * boundaryX, boundaryX)+offsetX;
+            //var a = map(x, 0, width, -boundary, boundary)-0.5+offsetX;
+            var b = map(y, 0, height, -boundaryY, boundaryY)+offsetY;
             var n;
             var x1 = a;
             var x2 = b;
